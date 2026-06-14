eb8a636c2ab579525fbcc78d30a9980a4125adee
imrod1n <imrod1n@yandex.ru>
2026-06-14 14:13:28 +0300

    correct links

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,11 +16,7 @@ def env_bool(name: str, default: str = 'False') -> bool:
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'dev-secret-change-in-production')
 DEBUG = False
-ALLOWED_HOSTS = ['https://koshkindom-u7zj.onrender.com']
-
-CORS_ALLOWED_ORIGINS = [
-    'https://koshkin-dom.onrender.com',  # например, https://example.com
-]
+ALLOWED_HOSTS = ['koshkindom-u7zj.onrender.com']
 
 if not DEBUG:
     _weak_keys = {'dev-secret-change-in-production', 'change-me-in-production'}
