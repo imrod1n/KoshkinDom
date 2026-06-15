b0c359a4692e9a47dbb8d01b4e3971edb31084fb
imrod1n <imrod1n@yandex.ru>
2026-06-15 22:47:37 +0300

    deploy

--- frontend/src/pages/FeedPage.jsx
+++ frontend/src/pages/FeedPage.jsx
@@ -70,7 +70,7 @@ export default function FeedPage() {
 
           <div className="col-md-6">
             <div className="card d-flex mb-4 shadow-sm">
-              <img src={MyImage} class="card-img" alt="" />
+              <img src={MyImage} className="card-img"/>
                 <h2 className="card-title">Добро пожаловать в кошкин дом - единое место для владельwев кошек</h2>
                 <p>Здесь собрано вместе всё, что вам нужно: чаты, статьи, обсуждения, напоминания о здоровье питомцев, ии-помощник</p>
               </div>
